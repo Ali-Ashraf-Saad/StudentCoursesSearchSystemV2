9dc6a11802306cd2540c776be7164cfe5bc0763d
Ali-Ashraf-Saad <Ali.ashraf9689@gmail.com>
2026-06-07 01:07:47 +0300

    bug fix in RUN.sh in cd

--- codePyData/RUN.sh
+++ codePyData/RUN.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# bash RUN.sh
-cd ...
 rm -rf data/courses.json data/exams.json data/rooms.json data/students.json foldersData txtData/subjects.txt txtData/All.txt txtData/rooms.txt 
 python3 -u "codePyData/AllCode.py"
 python3 -u "codePyData/rooms.py"
